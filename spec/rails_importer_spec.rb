97e8c41f2a2581663a60296d3b4446338234f12c
Sebastián Vicencio <sivicencio@gmail.com>
2016-02-08 16:01:32 -0300

    Update basic engine spec

--- spec/rails_importer_spec.rb
+++ spec/rails_importer_spec.rb
@@ -4,8 +4,4 @@ describe RailsImporter do
   it 'has a version number' do
     expect(RailsImporter::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
